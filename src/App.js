f819f4d40d81e0d22553e26f7a0d4f0fc43ba4d4
Stefan Hadzhiev <stefanhadzhiev24@gmail.com>
2024-04-28 21:17:24 +0300

    Static skeleton of forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,11 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -60,7 +64,48 @@ function Friend({ friend }) {
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🤼 Friend name </label>
+      <input type="text"></input>
+
+      <label>🖼️ Image Url </label>
+      <input type="text"></input>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text"></input>
+
+      <label>👦 Your expense</label>
+      <input type="text"></input>
+
+      <label>🤼X's expense</label>
+      <input disabled type="text"></input>
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X's name</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
 }
